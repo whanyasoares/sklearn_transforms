164f87b7c3c9aff250bd0d71ab6bcf758e75b6ad
whanyasoares <69917760+whanyasoares@users.noreply.github.com>
2020-08-21 11:30:08 -0300

    Update sklearn_transformers.py
    Adicionado novo imputer

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -8,9 +8,27 @@ class DropColumns(BaseEstimator, TransformerMixin):
 
     def fit(self, X, y=None):
         return self
-
+    
     def transform(self, X):
         # Primeiro realizamos a cópia do dataframe 'X' de entrada
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
+
+class ReplaceImputer(BaseEstimator, TransformerMixin):
+    def __init__(self, column_replace, column_compare, value_compare, replacer):
+        self.column_replace = column_replace
+        self.column_compare = column_compare
+        self.value_compare = value_compare
+        self.replacer = replacer
+
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X):
+        data = X.copy()
+
+        data.loc[data[self.column_compare] > self.value_compare, self.column_replace] = self.replacer
+        data[self.column_replace].fillna(data[self.column_replace].median(), inplace=True)
+
+        return data
